b1a9bcba0054520763cc3645e9878c899ed91a8c
im-amit-kumar <67156610+im-amit-kumar@users.noreply.github.com>
2022-11-13 18:34:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+﻿from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+main = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/modelacc99_val_96_5.h5'
+
+#Load your trained model
+model = load_model(MODEL_PATH)
+#model._make_predict_function()          # Necessary to make everything ready to run on the GPU ahead of time
+print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224,224)) #target_size must agree with what the trained model expects!!
+
+    # Preprocessing the image
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.
+
+    preds = model.predict(img)
+    pred = np.argmax(preds,axis = 1)
+    return pred
+
+
+@main.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@main.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        pred = model_predict(file_path, model)
+        os.remove(file_path)#removes file from the server after prediction has been returned
+        #predict = np.argmax(pred, axis=0)
+
+        # Arrange the correct return according to the model. 
+		# In this model 1 is Pneumonia and 0 is Normal.
+        str1 = 'Real'
+        str2 = 'Forged'
+        if pred[0] == 0:
+            return str1
+        else:
+            return str2
+    return None
+
+    #this section is used by gunicorn to serve the app on Heroku
+if __name__ == '__main__':
+        main.run()
+    #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
+    # Serve the app with gevent p
+    #http_server = WSGIServer(('', 5000), main)
+    #http_server.serve_forever()
